0b389f0078d183cd037d1e0796af20ddfc5922ee
duartequintela <duarte.quintela@tradingeconomics.com>
2019-01-22 14:19:24 +0000

    getForecastData fix

--- python/test.py
+++ python/test.py
@@ -1,6 +1,8 @@
 import tradingeconomics as te
 
-te.login('guest:guest') 
+te.login('n79l8wejd56zm4j:ohsu9f1psrdl43y') 
 
-a = te.getForecastData(country = 'commodity', indicator='copper', output_type = 'df')
+a = te.getForecastData(indicator = 'interbank rate', output_type = 'df')
 print (a)
+
+#https://tradingeconomics.com/api/indicators.aspx
