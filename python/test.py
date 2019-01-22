0d671504138bf9a7aed398cf5e5bb36879faa3b9
duartequintela <duarte.quintela@tradingeconomics.com>
2019-01-22 14:21:09 +0000

    .

--- python/test.py
+++
@@ -1,8 +0,0 @@
-import tradingeconomics as te
-
-te.login('n79l8wejd56zm4j:ohsu9f1psrdl43y') 
-
-a = te.getForecastData(indicator = 'interbank rate', output_type = 'df')
-print (a)
-
-#https://tradingeconomics.com/api/indicators.aspx
