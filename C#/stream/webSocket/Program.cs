24be10e2e54231727e282f42245b24b195da6129
Hitu Baldaniya <hitu.baldaniya@gmail.com>
2019-03-22 16:26:08 +0000

    streaming c# eg

--- C#/stream/webSocket/Program.cs
+++ C#/stream/webSocket/Program.cs
@@ -7,17 +7,21 @@ namespace webSocket
     {
         static void Main(string[] args)
         {
+            //only single market topic "EURUSD:CUR" can subscribe using guest:guest
+            //For other markets or calendar topic "key:secret" is required
             using (var ws = new WebSocket("ws://stream.tradingeconomics.com/?client=guest:guest"))
             {
+
                 ws.OnMessage += (sender, e) =>
-                    Console.WriteLine("Receiving : " + e.Data);
+                    Console.WriteLine("Receiving : " + e.Data); //Streaming data
 
                 ws.Connect(); //Connecting to the websocket
 
                 Console.WriteLine("Connected to the socket");
 
                 //Calendar is not frequent, need to wait until the data comes
-                ws.Send("{\"topic\": \"subscribe\", \"to\": \"calendar\" }"); //Subscribing to the calendar
+                //Use your "key:secret" to receive calendar streaming data
+                ws.Send("{\"topic\": \"subscribe\", \"to\": \"calendar\" }"); //Subscribing the topic named calendar
 
                 Console.ReadKey(true);
 
