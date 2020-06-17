7299af629a1cc48709ccf4d79f1f26c2f3a89b9e
patriciavicente <patricia.martins@tradingeconomics.com>
2020-06-17 11:58:47 +0100

    bug fix on getHistorical

--- python/tradingeconomics/historicalDB.py
+++ python/tradingeconomics/historicalDB.py
@@ -85,9 +85,9 @@ def getHistorical(symbol = None, initDate = None, endDate = None, output_type = 
         linkAPI += 'historical/ticker/' + quote(symbol, safe='')
                 
     elif (':' in symbol): 
-        linkAPI += 'markets/historical/' + quote(symbol, safe='";" | "/" | "?" | ":" | "@" | "&" | "=" | "+" | "$" | "," | "~"') 
+        linkAPI += 'markets/historical/' + quote(symbol, safe='') 
     else:
-        linkAPI += 'markets/historical/' + quote(','.join(symbol), safe='";" | "/" | "?" | ":" | "@" | "&" | "=" | "+" | "$" | "," | "~"')    
+        linkAPI += 'markets/historical/' + quote(','.join(symbol), safe='')     
     
     if (initDate is not None) and (endDate is not None):
         try: 
