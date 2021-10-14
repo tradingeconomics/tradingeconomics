90c9d3e32c7f5d078988816030f203ec9d8dbcd0
FGuerreir0 <ffsguerreiro@hotmail.com>
2021-10-14 09:22:37 +0100

    Added Indicators examples

---
+++ nodejs/Examples/Indicators/indicators.js
@@ -0,0 +1,46 @@
+// DOCUMENTATION:
+// http://docs.tradingeconomics.com/?javascript#indicators
+
+const te = require('tradingeconomics');
+
+// Login with client key or leave it blank, you can get your free key here: http://developer.tradingeconomics.com
+// Note: without a client key only a small sample of data will be given.
+te.login(); 
+
+//===============================================================================================================
+// Get a list of all indicators
+
+te.getIndicatorData().then(all => {
+  console.log('List of all Indicators', '\n', all, '\n',)       
+}).catch(err => console.log(err));
+
+//===============================================================================================================
+// Get an indicators list by country or countries
+
+te.getIndicatorData(country = ['united states','china']).then(country => {
+  console.log('List of indicators by country', '\n', country, '\n',)       
+}).catch(err => console.log(err))
+
+//===============================================================================================================
+// Get an indicators list by country and specific group
+
+  te.getIndicatorData(country = 'china', group = 'housing').then(countryGroup => {
+    console.log('List of indicators by country', '\n', countryGroup, '\n',)       
+  }).catch(err => console.log(err))
+
+//===============================================================================================================
+// Get specific indicator for all countries 
+
+  te.getIndicatorData(indicator = 'gdp').then(indicator => {
+    console.log('List of a specific indicator', '\n', indicator, '\n',)       
+  }).catch(err => console.log(err))
+  
+//===============================================================================================================
+// Get a list of indicators by ticker
+
+  te.getIndicatorData(ticker = 'usurtot').then(ticker => {
+    console.log('List of indicators by ticker', '\n', ticker, '\n',)       
+  }).catch(err => console.log(err))
+
+
+
