506106961fcd08de3913594c98378565ee7f0845
Tatenda <tatenda@tradingeconomics.com>
2023-03-29 19:18:50 +0200

    -

--- python/tests/test_comtrade.py
+++ python/tests/test_comtrade.py
@@ -3,13 +3,13 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import tradingeconomics as te
-te.login('912A151D51F9485:98FA4E298F6B4D0')
+te.login('guest:guest')
 
 class TestgetCmtTotalByTypeAndMainCategory(unittest.TestCase):
     
     def test_getCmtTotalByTypeAndMainCategory(self):
-        a = te.getCmtTotalByTypeAndMainCategory(country = 'India', type='import',output_type = 'df')
-        self.assertEqual(a['country'][0], 'India')
+        a = te.getCmtTotalByTypeAndMainCategory(country = 'United kingdom', type='import',output_type = 'df')
+        self.assertEqual(a['country'][0], 'United Kingdom')
     
     def test_getCmtTotalByTypeAndMainCategory2(self):
         a = te.getCmtTotalByTypeAndMainCategory(country = 'India', type='import',output_type = 'df')
