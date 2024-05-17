67454764d3b29b8110e32707ac84d07d57ca2f7c
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2024-05-18 01:37:46 +0530

    udpate

--- test-project/src/app/project/3/page.tsx
+++ test-project/src/app/project/3/page.tsx
@@ -180,7 +180,7 @@ export default function ProjectThree() {
         </Card>
 
         {data  ? (
-          <div className="flex flex-col  items-center justify-center rounded-lg border border-dashed shadow-sm  ">
+          <div className="flex flex-col  items-center justify-center rounded-lg border border-dashed shadow-sm py-20 ">
             {isLoading ? (
               <Loader2Icon className="ml-2 h-10 w-10 animate-spin" />
             ) : (
