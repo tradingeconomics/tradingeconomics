a162d37489582493887363d4a6d3f816f36e4d7a
Chima Ilo <collins.chima.ilo@gmail.com>
2024-09-17 04:55:19 +0100

    update dockerfile

--- typescript/vite.config.ts
+++ typescript/vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 })
