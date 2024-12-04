ead81ed3566143ab43729b609e5777d7a4701b2a
Ammar <150323504+ammarproficient@users.noreply.github.com>
2024-12-04 17:30:43 +0500

    Delete trading.py

--- trading.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Your Trading Economics API Key
-API_KEY = "1e92856abb004fd:s1zhxexpoawwfvs"  # Replace with os.getenv("TRADING_ECONOMICS_API_KEY") for security
-
-
-url = f'https://api.tradingeconomics.com/markets/historical/aapl:us,gac:com?c={API_KEY}'
-data = requests.get(url).json()
-print(data)
