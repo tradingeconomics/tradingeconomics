04adfddb8abc389265892dbfc9fe5fc1366ab67f
Jude Ukana <judeukana@gmail.com>
2024-12-24 05:00:56 +0100

    angular app added

---
+++ angular/CategoryHistoricalDisplayApp/src/app/data-table/data-table.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DataTableComponent } from './data-table.component';
+
+describe('DataTableComponent', () => {
+  let component: DataTableComponent;
+  let fixture: ComponentFixture<DataTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DataTableComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DataTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
