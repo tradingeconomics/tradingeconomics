7f4b7996431187687827b6595dfb4b3e97d6d653
Alexandre Ferreira <alexjorgef@protonmail.com>
2026-06-13 19:52:04 +0100

    initial commit of portfolio_manager lib example
    - added project structure based on dart create CLI v3.12.1 (package template)
    
    Signed-off-by: Alexandre Ferreira <alexjorgef@protonmail.com>

---
+++ dart/examples/portfolio_manager_lib/example/portfolio_manager_lib_example.dart
@@ -0,0 +1,6 @@
+import 'package:portfolio_manager_lib/portfolio_manager_lib.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart/examples/portfolio_manager_lib/test/portfolio_manager_lib_test.dart
@@ -0,0 +1,16 @@
+import 'package:portfolio_manager_lib/portfolio_manager_lib.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
