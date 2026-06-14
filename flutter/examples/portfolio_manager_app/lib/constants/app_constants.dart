c3a3c118850be6ac1c0d8a843e0838265dc62827
Alexandre Ferreira <alexjorgef@protonmail.com>
2026-06-14 19:08:37 +0100

    demo: add app constants
    Signed-off-by: Alexandre Ferreira <alexjorgef@protonmail.com>

---
+++ flutter/examples/portfolio_manager_app/lib/constants/app_constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const double kPadding = 16.0;
+}
+
+class AppColors {
+  static const primaryBlue = Color(0xFF1E88E5);
+  static const darkBg = Color(0xFF0F172A);
+  static const cardBg = Color(0xFF1E293B);
+
+  static const accent = Color(0xFF38BDF8);
+  static const appBarBg = Color(0xFF0B1220);
+}
