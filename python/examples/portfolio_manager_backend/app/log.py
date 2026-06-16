45fde8486e9a4607549cba467fbbbf72ab9ae5c7
Alexandre Ferreira <alexjorgef@protonmail.com>
2026-06-16 03:41:25 +0100

    chore: fix duplicated logger creation
    Signed-off-by: Alexandre Ferreira <alexjorgef@protonmail.com>

--- python/examples/portfolio_manager_backend/app/log.py
+++ python/examples/portfolio_manager_backend/app/log.py
@@ -1,15 +1,17 @@
 import logging
 
 
-def setup_custom_logger(name):
-    formatter = logging.Formatter(
-        fmt="%(asctime)s - %(levelname)s - %(module)s - %(message)s"
-    )
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-
+def setup_custom_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            fmt="%(asctime)s - %(levelname)s - %(module)s - %(message)s"
+        )
+
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
     return logger
